ca8f408865abf8f36e8b38d8c38fb551236218db
Joseruben.castro <joseruben.castro@bestsecret.com>
2021-01-11 13:05:54 +0100

    Refactor MainActivity
    - Rename ViewModel to MainActivityViewModel and use it for this activity
    - Add get methods for all properties that we want to display on the screen
    - Add set method for the remember property because we want to modify it
    - Add listener to the checkbox for update the model when the view changes
    - Add listener to the button to change the data and update the ui elements manually

---
+++ app/src/main/java/com/example/mvvmsample/MainActivityViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.mvvmsample
+
+import android.util.Log
+import androidx.databinding.ObservableBoolean
+
+class MainActivityViewModel {
+    private val userDetailsModel = UserDetailsModel()
+    private var user: User
+    val remember = ObservableBoolean(false)
+
+    init {
+        user = userDetailsModel.login()
+        remember.set(user.rememberMe)
+    }
+
+    fun getName(): String {
+        return user.name
+    }
+
+    fun getAge(): String {
+        return user.age
+    }
+
+    fun setRememberMe() {
+        when (user.rememberMe) {
+            true -> {
+                user.rememberMe = false
+                remember.set(false)
+            }
+            false -> {
+                user.rememberMe = true
+                remember.set(true)
+            }
+        }
+        Log.d("i", "user.rememberMe value is " + user.rememberMe)
+    }
+
+    fun getRememberMe(): Boolean {
+        return user.rememberMe
+    }
+
+    fun changeUser() {
+        when (user.name) {
+            "Pepito" -> user = User(
+                "Margarita",
+                "22",
+                false
+            )
+            "Margarita" -> user = User(
+                "Pepito",
+                "31",
+                true
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/mvvmsample/activities/MainActivity.kt
+++ app/src/main/java/com/example/mvvmsample/activities/MainActivity.kt
@@ -2,23 +2,42 @@ package com.example.mvvmsample.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
+import com.example.mvvmsample.MainActivityViewModel
 import com.example.mvvmsample.R
-import com.example.mvvmsample.ViewModel
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel = ViewModel()
+    private val viewModel = MainActivityViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        updateUIWithViewModelData()
+
+        findViewById<CheckBox>(R.id.checkBox).setOnClickListener {
+            viewModel.setRememberMe()
+        }
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            viewModel.changeUser()
+            updateUIWithViewModelData()
+        }
+    }
+
+    private fun updateUIWithViewModelData() {
         findViewById<TextView>(R.id.textName).apply {
             text = viewModel.getName()
         }
 
         findViewById<TextView>(R.id.textAge).apply {
             text = viewModel.getAge()
+        }
+
+        findViewById<CheckBox>(R.id.checkBox).apply {
+            isChecked = viewModel.getRememberMe()
         }
     }
 }
